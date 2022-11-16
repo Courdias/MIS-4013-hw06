11c50aaa2c4a3f893187d3349431097000fc0c5a
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-15 18:56:56 -0600

    Update library1.php

--- library1.php
+++ library1.php
@@ -1,20 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<meta charset="UTF-8">
-<title>Library 1</title>
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<style>
-</style>
-<script src="https://www.w3schools.com/lib/w3.js"></script>
-<body>
-
-<img class="nature" src="img_snowtops.jpg" width="100%">
-<img class="nature" src="img_mountains.jpg" width="100%">
-<img class="nature" src="img_nature.jpg" width="100%">
+<?php
+  include "header.php";
+?>
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+<div id="myPlot" style="width:100%;max-width:700px"></div>
 
 <script>
-w3.slideshow(".nature", 1500);
+var xArray = [50,60,70,80,90,100,110,120,130,140,150];
+var yArray = [7,8,8,9,9,9,10,11,14,14,15];
+
+// Define Data
+var data = [{
+  x:xArray,
+  y:yArray,
+  mode:"markers"
+}];
+
+// Define Layout
+var layout = {
+  xaxis: {range: [40, 160], title: "Square Meters"},
+  yaxis: {range: [5, 16], title: "Price in Millions"},  
+  title: "House Prices vs. Size"
+};
+
+// Display using Plotly
+Plotly.newPlot("myPlot", data, layout);
 </script>
 
 </body>
